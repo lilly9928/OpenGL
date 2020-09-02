9d5735c8bc43e31bc55c61d9a71e8cc3b2e2c36e
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2020-09-02 10:34:44 +0900

    뮤

---
+++ opengl_study/2_1_opengl/2_1_opengl/hello_window.hpp
@@ -0,0 +1,14 @@
+//
+//  hello_window.hpp
+//  2_1_opengl
+//
+//  Created by kawaii on 2020/09/01.
+//  Copyright © 2020 kawaii. All rights reserved.
+//
+
+#ifndef hello_window_hpp
+#define hello_window_hpp
+
+#include <stdio.h>
+
+#endif /* hello_window_hpp */
