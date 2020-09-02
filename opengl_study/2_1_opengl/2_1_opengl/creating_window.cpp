222f521bfb46267fde6779331943a3ff5f322dcc
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2020-09-02 16:42:20 +0900

    1

--- opengl_study/2_1_opengl/2_1_opengl/creating_window.cpp
+++ opengl_study/2_1_opengl/2_1_opengl/creating_window.cpp
@@ -14,25 +14,20 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
-// settings
+//윈도우 창의 높이 넓이
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
+    glfwInit();  //glwf초기화
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
+    //맥os에서는 추가해줘야함
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
 
-    // glfw window creation
-    // --------------------
+   //원도우 객체 생성
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -42,48 +37,42 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
+    
+    //glad opengl용 함수포인터
+    //glfwGetProcAddress 컴파일 할 환경의 os
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // render loop
-    // -----------
+    //render loop 이미지를 그리고 바로 꺼지는 것을 막기위해서 loop를 돌려줘야함
+    //glfwWindowShouldClose :응용프로그램을 닫았는지
+    //glfwPollEvents : 이벤트가 발생했는지 확인 , 윈도우 상태 업데이트 , 콜백함수 호출
+    //glfwSwapBuffers : 컬러버퍼를 교체
+    //----------------------------------------------------------------
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        // -----
         processInput(window);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
+    //할당한 자원 정리
     glfwTerminate();
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
+    //키보드 키 입력 받기 ,,esc 버튼 눌렸는지 확인 
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+//창의 크기가 변경될때 마다 적절한 데이터 채우기
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
